6573be70ba2894f5da8957dbde9914192ce1c169
matija.mazi@gmail.com <matija.mazi@gmail.com>
2013-06-13 12:54:47 +0200

    minor model stuff

--- model/src/main/java/si/gov/mnz/blp/taxid/model/Member.java
+++ model/src/main/java/si/gov/mnz/blp/taxid/model/Member.java
@@ -50,15 +50,10 @@ public class Member implements Serializable {
     @NotNull
     @Size(min = 10, max = 12)
     @Digits(fraction = 0, integer = 12)
-    @Column(name = "phone_number")
     private String phoneNumber;
 
     public Long getId() {
         return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public String getName() {
