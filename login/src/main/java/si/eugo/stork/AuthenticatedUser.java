74d41628351a1b8820ddfc09c26308e264eac2d3
matija.mazi@gmail.com <matija.mazi@gmail.com>
2013-07-01 13:32:36 +0200

    Move logic from servlet to CDI bean. This should help prevent problems with multiple threads accessing the same servlet (though probably not prevent them completely).

--- login/src/main/java/si/eugo/stork/AuthenticatedUser.java
+++ login/src/main/java/si/eugo/stork/AuthenticatedUser.java
@@ -1,9 +1,22 @@
 package si.eugo.stork;
 
+import eu.stork.peps.auth.commons.IPersonalAttributeList;
+import eu.stork.peps.auth.commons.PEPSUtil;
+import eu.stork.peps.auth.commons.PersonalAttribute;
+import eu.stork.peps.auth.commons.STORKAuthnResponse;
+import eu.stork.peps.auth.engine.STORKSAMLEngine;
+import eu.stork.peps.exceptions.STORKSAMLEngineException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.enterprise.context.SessionScoped;
 import javax.enterprise.inject.Produces;
 import javax.inject.Named;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author Matija Mazi <br/>
@@ -11,10 +24,13 @@ import java.io.Serializable;
  */
 @SessionScoped
 public class AuthenticatedUser implements Serializable {
+    private static final Logger log = LoggerFactory.getLogger(AuthenticatedUser.class);
+
     private StorkUser user;
 
-    public void login(StorkUser user) {
-        this.user = user;
+    private static String getAttribute(Map<String, List<String>> nameValueMap, String key) {
+        List<String> values = nameValueMap.get(key);
+        return values == null || values.isEmpty() ? null : values.get(0);
     }
 
     public void logout() {
@@ -24,5 +40,58 @@ public class AuthenticatedUser implements Serializable {
     @Produces @Authenticated @Named("storkUser")
     public StorkUser getUser() {
         return user;
+    }
+
+    public void login(String samlResponse, String remoteHost) {
+        STORKAuthnResponse authnResponse;
+        IPersonalAttributeList personalAttributeList;
+
+        byte[] decSamlToken = PEPSUtil.decodeSAMLToken(samlResponse);
+
+        STORKSAMLEngine engine = STORKSAMLEngine.getInstance(Constants.SP_CONF);
+
+        try {
+            // validate SAML Token
+            authnResponse = engine.validateSTORKAuthnResponse(decSamlToken, remoteHost);
+        } catch (STORKSAMLEngineException e) {
+            throw new ApplicationSpecificServiceException("Could not validate token for Saml Response", e.toString());
+        }
+
+        if (authnResponse.isFail()) {
+            throw new ApplicationSpecificServiceException("Saml Response is fail", authnResponse.getMessage());
+        }
+        StorkUser storkUser = new StorkUser();
+        // Get attributes
+        personalAttributeList = authnResponse.getPersonalAttributeList();
+        ArrayList<PersonalAttribute> attrList = new ArrayList<PersonalAttribute>(
+                personalAttributeList.values());
+        Map<String, PersonalAttribute> storkAttrs = new HashMap<String, PersonalAttribute>();
+        Map<String, List<String>> nameValueMap = new HashMap<String, List<String>>();
+        for (PersonalAttribute pa : attrList) {
+            log.debug("PersonalAttribute Friendly Name = " + pa.getFriendlyName());
+            log.debug("PersonalAttribute Name = " + pa.getName());
+            if (pa.getValue() != null) {
+                storkAttrs.put(pa.getName(), pa);
+                if (pa.getValue() != null) {
+                    nameValueMap.put(pa.getName(), pa.getValue());
+                }
+            }
+        }
+
+        storkUser.setDateOfBirth(getAttribute(nameValueMap, "dateOfBirth"));
+        storkUser.setSurname(getAttribute(nameValueMap, "surname"));
+        storkUser.setGivenName(getAttribute(nameValueMap, "givenName"));
+        storkUser.seteId(getAttribute(nameValueMap, "eIdentifier"));
+        storkUser.setUsername(getAttribute(nameValueMap, "eIdentifier"));
+        storkUser.setTextResidenceAddress(getAttribute(nameValueMap, "textResidenceAddress"));
+        storkUser.setCanonicalResidenceAddress(getAttribute(nameValueMap, "canonicalResidenceAddress"));
+        storkUser.setGender(getAttribute(nameValueMap, "gender"));
+        storkUser.setNationalityCode(getAttribute(nameValueMap, "nationalityCode"));
+        storkUser.setCountryCodeOfBirth(getAttribute(nameValueMap, "countryCodeOfBirth"));
+
+        storkUser.setStorkAttrs(storkAttrs);
+
+        log.debug("Logged in: {}", storkUser);
+        this.user = storkUser;
     }
 }
--- login/src/main/java/si/eugo/stork/StorkAuthServlet.java
+++ login/src/main/java/si/eugo/stork/StorkAuthServlet.java
@@ -1,11 +1,5 @@
 package si.eugo.stork;
 
-import eu.stork.peps.auth.commons.IPersonalAttributeList;
-import eu.stork.peps.auth.commons.PEPSUtil;
-import eu.stork.peps.auth.commons.PersonalAttribute;
-import eu.stork.peps.auth.commons.STORKAuthnResponse;
-import eu.stork.peps.auth.engine.STORKSAMLEngine;
-import eu.stork.peps.exceptions.STORKSAMLEngineException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,10 +10,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class StorkAuthServlet extends HttpServlet {
 
@@ -44,71 +34,8 @@ public class StorkAuthServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 
-		STORKAuthnResponse authnResponse;
-		IPersonalAttributeList personalAttributeList;
-
-
-		byte[] decSamlToken = PEPSUtil.decodeSAMLToken(req.getParameter("SAMLResponse"));
-
-		STORKSAMLEngine engine = STORKSAMLEngine.getInstance(Constants.SP_CONF);
-
-		try {
-			// validate SAML Token
-			authnResponse = engine.validateSTORKAuthnResponse(decSamlToken, req.getRemoteHost());
-		} catch (STORKSAMLEngineException e) {
-			throw new ApplicationSpecificServiceException(
-					"Could not validate token for Saml Response",
-					e.toString());
-		}
-
-		if (authnResponse.isFail()) {
-			throw new ApplicationSpecificServiceException(
-					"Saml Response is fail", authnResponse.getMessage());
-		} else {
-
-			StorkUser storkUser = new StorkUser();
-			// Get attributes
-			personalAttributeList = authnResponse.getPersonalAttributeList();
-            ArrayList<PersonalAttribute> attrList = new ArrayList<PersonalAttribute>(
-                    personalAttributeList.values());
-			Map<String, PersonalAttribute> storkAttrs = new HashMap<String, PersonalAttribute>();
-			Map<String, List<String>> nameValueMap = new HashMap<String, List<String>>();
-			for (PersonalAttribute pa : attrList) {
-				log.debug("PersonalAttribute Friendly Name = "
-                        + pa.getFriendlyName());
-				log.debug("PersonalAttribute Name = " + pa.getName());
-				if (pa.getValue() != null) {
-					storkAttrs.put(pa.getName(), pa);
-					if (pa.getValue() != null)
-						nameValueMap.put(pa.getName(), pa.getValue());
-				}
-			}
-
-            storkUser.setDateOfBirth(getAttribute(nameValueMap, "dateOfBirth"));
-            storkUser.setSurname(getAttribute(nameValueMap, "surname"));
-            storkUser.setGivenName(getAttribute(nameValueMap, "givenName"));
-            storkUser.seteId(getAttribute(nameValueMap, "eIdentifier"));
-            storkUser.setUsername(getAttribute(nameValueMap, "eIdentifier"));
-            storkUser.setTextResidenceAddress(getAttribute(nameValueMap, "textResidenceAddress"));
-            storkUser.setCanonicalResidenceAddress(getAttribute(nameValueMap, "canonicalResidenceAddress"));
-            storkUser.setGender(getAttribute(nameValueMap, "gender"));
-            storkUser.setNationalityCode(getAttribute(nameValueMap, "nationalityCode"));
-            storkUser.setCountryCodeOfBirth(getAttribute(nameValueMap, "countryCodeOfBirth"));
-
-            storkUser.setStorkAttrs(storkAttrs);
-
-            authenticatedUser.login(storkUser);
-
-            //TODO Decide on JAAS
-			//req.login(storkUser.getUsername(), storkUser.geteId());
-			
-			resp.sendRedirect(successPage);
-		}
-	}
-
-    private String getAttribute(Map<String, List<String>> nameValueMap, String dateOfBirth) {
-        List<String> values = nameValueMap.get(dateOfBirth);
-        return values == null || values.isEmpty() ? null : values.get(0);
+        authenticatedUser.login(req.getParameter("SAMLResponse"), req.getRemoteHost());
+        resp.sendRedirect(successPage);
     }
 
     @Override
